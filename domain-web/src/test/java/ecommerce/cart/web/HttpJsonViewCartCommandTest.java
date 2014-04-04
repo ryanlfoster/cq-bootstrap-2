350a6914dc45c7ebc485cd314bfa3df2bc8a3e66
Stephen Smithstone <stephen@smithstone.co.uk>
2014-04-04 20:53:39 +1100

    this got added to the build might as well leave it in for now

--- domain-web/src/test/java/ecommerce/cart/web/HttpJsonViewCartCommandTest.java
+++ domain-web/src/test/java/ecommerce/cart/web/HttpJsonViewCartCommandTest.java
@@ -1,5 +1,6 @@
 package ecommerce.cart.web;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -10,7 +11,7 @@ import static org.junit.Assert.assertThat;
 public class HttpJsonViewCartCommandTest {
 
 
-    @Test
+    @Test @Ignore("yet to implement")
     public void xsxsx() throws Exception {
         MockHttpServletRequest request = new MockHttpServletRequest();
         MockHttpServletResponse response = new MockHttpServletResponse();
