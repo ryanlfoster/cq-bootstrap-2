b1c938c525ade41fe46d57571a7d481734170e17
Stephen Smithstone <stephen@smithstone.co.uk>
2014-04-06 20:21:23 +1000

    created vagrant puppet configured author and publisher environments

---
+++ environments/devops/puppet/manifests/dev.author.pp
@@ -0,0 +1,33 @@
+
+
+
+node 'author1.smithstone.local' {
+  include apt
+  apt::ppa { 'ppa:webupd8team/java': }
+  package { "vim" : ensure => 'present'  }
+  package { "nano" : ensure => 'absent'  }
+  package { "tmux" : ensure => 'present'  }
+
+  file { "/home/vagrant/aem" : ensure => 'directory' , owner => "vagrant" , group => "vagrant" }
+  ->
+  file { "/home/vagrant/aem/author" : ensure => 'directory' , owner => "vagrant" , group => "vagrant" }
+  ->
+  file { "/home/vagrant/aem/author/license.properties" : ensure => 'present' ,
+    content => "license.product.name=Adobe Experience Manager
+license.customer.name=Day Training
+license.product.version=5.6.1.20130606
+license.downloadID=16c35d55-6a3d-8a48-986c-9d1be7ebbf70
+
+    ", owner => "vagrant" , group => "vagrant"
+
+  }
+  ->
+  file { "/home/vagrant/aem/run_cq.sh" :
+    ensure => 'present',
+    content => "#!/bin/bash
+java -Djava.awt.headless=true -Xms1024m -Xmx2048M -XX:MaxPermSize=512M -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1045 -jar /aem_data/cq-quickstart-5.6.1.jar -nobrowser -r dev,author -p 4502
+
+", owner => "vagrant" , group => "vagrant"
+  }
+
+}
---
+++ environments/general/README.md
@@ -0,0 +1,2 @@
+This directory should contain cq-quickstart-5.6.1.jar
+
